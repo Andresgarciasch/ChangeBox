a337957cec3745d22844eae59cef9bef4824aa7e
Knarfg <frankgonzalez033@gmail.com>
2022-06-24 01:03:24 +0000

    23junio

--- src/api/models.py
+++ src/api/models.py
@@ -4,19 +4,23 @@ db = SQLAlchemy()
 
 # Datos de usuarios
 class User(db.Model):
+    # Registro
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), unique=True, nullable=False)
     lastname = db.Column(db.String(120), unique=True, nullable=False)
     username = db.Column(db.String(120), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
+    # Verificación
+    birthday = db.Column(db.DateTime, unique=False, nullable=False)
     identification = db.Column(db.String(120), unique=True, nullable=False)
     nationality = db.Column(db.String(120), unique=True, nullable=False)
-    birthday = db.Column(db.DateTime, unique=False, nullable=False)
     # attached_file = db.Column(db.XXXXXX, unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    # Funcionamiento
     reputation = db.Column(db.Float, unique=True, nullable=False)
     completed_trades = db.Column(db.Integer, unique=True, nullable=False)
+    score = db.Column(db.Integer, unique=True, nullable=False)
     # Añadir relación con "historial de transacciones" (dos usuarios por transacción - relación varios a uno)
     # Añadir relación con "compra" (dos usuarios por transacción - relación uno a varios)
     # Añadir relación con "venta" (dos usuarios por transacción - relación uno a varios)
@@ -24,15 +28,30 @@ class User(db.Model):
     def __repr__(self):
         return f'<User {self.email}>'
 
-    def serialize(self):
-        return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
-        }
+    # def create(self):
+    #     try:
+    #         db.session.add(self)
+    #         db.session.commit()
+    #         return True
+    #     except:
+    #         db.session.rollback()
+    #         print('An error has ocurred')
+    #         return False
+    
+    @classmethod
+    def create(cls, data):
+        try:
+            new_user = cls(**data)
+            db.session.add(new_user)
+            db.session.commit()
+            return new_user
+        except Exception as error:
+            db.session.rollback()
+            print(error)
+            return None
 
     # Se va a permitir que el usuario actualice sus datos?
-    # Cómo actualiza los datos si se rechaza la verificación? Se registra nuevamente?
+    # Cómo actualiza los datos si se rechaza la verificación?
     # Qué datos podra actualizar?
 
     # def update(self, new_name, new_lastname, new_username, new_nationality):
@@ -43,23 +62,31 @@ class User(db.Model):
     #     db.session.commit()
     #     return True
 
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+            # do not serialize the password, its a security breach
+        }
+
+
+
 
 
 # Transacción de compra
 class Buypublications(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.DateTime, unique=False, nullable=False)
     exchange_rate = db.Column(db.Float, unique=True, nullable=False)
     balance = db.Column(db.Float, unique=True, nullable=False)
     message = db.Column(db.String(280), unique=True, nullable=False)
     # preferred_banks puede provenir de una lista desplegable?
     # preferred_banks = 
-    # 3 status = [open, filled, canceled]
+    # 4 status = [open, trading, filled, canceled]
     status = db.Column(db.String(120), unique=True, nullable=False)
     # Añadir relación con usuario que compra
     # Añadir relación con usuario que vende
-    # Añadir relación con operaciones de compra
-    # Añadir relación con historial de publicaciones
-    # Añadir relación con historial de operaciones?
+    # Añadir relación con historial de operaciones
 
     def update(self, new_exchange_rate, new_balance, new_message, new_preferred_banks):
         self.exchange_rate = new_exchange_rate
@@ -69,16 +96,19 @@ class Buypublications(db.Model):
         db.session.commit()
         return True
 
-    def delete(self):
-        db.session.delete(self)
-        db.session.commit()
-        return True
+    # Más que borrar, se debería ocultar de la pizarra pero el registro debe quedar en base de datos
+    # Al cancelar la publicación cambia a estatus cancelado y
+    # def delete(self):
+    #     db.session.delete(self)
+    #     db.session.commit()
+    #     return True
 
     def serialize(self):
         return {
             "id": self.id,
             "date": self.date,
-            "api_id": self.api_id
+            "exchange_rate": self.exchange_rate,
+            "balance": self.balance
         }
 
 
@@ -87,18 +117,17 @@ class Buypublications(db.Model):
 # Transacción de venta
 class Sellpublications(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.DateTime, unique=False, nullable=False)
     exchange_rate = db.Column(db.Float, unique=True, nullable=False)
     balance = db.Column(db.Float, unique=True, nullable=False)
     message = db.Column(db.String(280), unique=True, nullable=False)
     # preferred_banks puede provenir de una lista desplegable?
     # preferred_banks = 
-    # 3 status = [open, filled, canceled]
+    # 4 status = [open, trading, filled, canceled]
     status = db.Column(db.String(120), unique=True, nullable=False)
     # Añadir relación con usuario que compra
     # Añadir relación con usuario que vende
-    # Añadir relación con operaciones de compra
-    # Añadir relación con historial de publicaciones
-    # Añadir relación con historial de operaciones?
+    # Añadir relación con historial de operaciones
 
     def update(self, new_exchange_rate, new_balance, new_message, new_preferred_banks):
         self.exchange_rate = new_exchange_rate
@@ -108,17 +137,38 @@ class Sellpublications(db.Model):
         db.session.commit()
         return True
 
-    def delete(self):
-        db.session.delete(self)
-        db.session.commit()
-        return True
+    # Más que borrar, se debería ocultar de la pizarra pero el registro debe quedar en base de datos
+    # Al cancelar la publicación cambia a estatus cancelado y
+    # def delete(self):
+    #     db.session.delete(self)
+    #     db.session.commit()
+    #     return True
 
     def serialize(self):
         return {
             "id": self.id,
             "date": self.date,
-            "api_id": self.api_id
+            "exchange_rate": self.exchange_rate,
+            "balance": self.balance
         }
 
 
 
+
+class Transactionhistory(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.DateTime, unique=False, nullable=False)
+    exchange_rate = db.Column(db.Float, unique=True, nullable=False)
+    balance = db.Column(db.Float, unique=True, nullable=False)
+    # 4 status = [open, trading, filled, canceled]
+    status = db.Column(db.String(120), unique=True, nullable=False)
+    # Añadir relación con publicaciones de compra
+    # Añadir relación con publicaciones de venta
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "date": self.date,
+            "exchange_rate": self.exchange_rate,
+            "balance": self.balance
+        }
