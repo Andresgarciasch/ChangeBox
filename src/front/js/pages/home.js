abd586b1ab12c9adcba6110820caef333d66c114
Andresgarciasch <andres_28garcia@outlook.es>
2022-07-08 21:23:01 +0000

    landing page

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,26 +1,27 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
+import LOGO1 from "../../img/LOGO 1.png";
 
 export const Home = () => {
   const { store, actions } = useContext(Context);
 
   return (
-    <div className="text-center mt-5">
-      <h1>Hello Rigo!!</h1>
+    <div className="fondo text-center">
+      <h1>ChangeBox</h1>
       <p>
-        <img src={rigoImageUrl} />
+        <img src={LOGO1} />
       </p>
-      <div className="alert alert-info">
+      {/* <div className="alert alert-info">
         {store.message ||
           "Loading message from the backend (make sure your python backend is running)..."}
-      </div>
-      <p>
+      </div> */}
+      {/* <p>
         This boilerplate comes with lots of documentation:{" "}
         <a href="https://github.com/4GeeksAcademy/react-flask-hello/tree/95e0540bd1422249c3004f149825285118594325/docs">
           Read documentation
-        </a>
+        </a> 
       </p>
+        */}
     </div>
   );
 };
